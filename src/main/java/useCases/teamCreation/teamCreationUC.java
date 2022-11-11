fe3bfeaee8fc9bc65aa49f33a3c8665525d9ced2
Kelly Ou <kelleo0703@gmail.com>
2022-11-10 23:34:45 -0500

    Modified teamCreationUC, instead of working with the leaves of the game tree, findBlankTeam looks for blank teams in the list of teams stored in the bracket.

--- src/main/java/useCases/teamCreation/teamCreationUC.java
+++ src/main/java/useCases/teamCreation/teamCreationUC.java
@@ -1,10 +1,7 @@
 package useCases.teamCreation;
 import java.util.ArrayList;
 import java.util.List;
-//import entities.DefaultUser;
-//import entities.DefaultTeam;
-//import entities.Team;
-//import entities.DefaultBracket;
+
 import entities.*;
 
 
@@ -16,10 +13,9 @@ public class teamCreationUC {
         return creator.getBracketRole(curBracket.getTournamentID()).equals("Player");
     }
 
-
-    //used to check if team with the same teamName exists in the first round
+    //used to check if a team with the same teamName exists in the bracket
     public boolean checkTeamNameExists(String teamName, DefaultBracket curBracket){
-        ArrayList<Team> teams = getAllTeams(curBracket);
+        ArrayList<Team> teams = curBracket.getTeams();
         for(Team team: teams){
             if(team.getTeamName().equals(teamName)){
                 return false;
@@ -28,30 +24,9 @@ public class teamCreationUC {
         return true;
     }
 
-    // helper method to get all the teams in the game tree
-    public ArrayList<Team> getAllTeams(DefaultBracket curBracket){
-        ArrayList<Game> games = getAllLeaves(curBracket.getFinalGame());
-        ArrayList<Team> teams = new ArrayList<Team>();
-        for(Game game: games){
-            teams.addAll(game.getTeams());
-        }
-        return teams;
-    }
-    // helper method to get all the leaves of the game tree
-    public ArrayList<Game> getAllLeaves(Game curGame){
-        ArrayList<Game> games =  new ArrayList<Game>();
-        if(curGame.getPrevGame1() == null && curGame.getPrevGame2() == null){
-            games.add(curGame);
-        } else {
-            games.addAll(getAllLeaves(curGame.getPrevGame1()));
-            games.addAll(getAllLeaves(curGame.getPrevGame2()));
-        }
-        return games;
-    }
-
-    //find a blank team in the game tree, returns null if the bracket is full
+    //find a blank team in the bracket, returns null if the bracket is full
     public Team findBlankTeam(DefaultBracket curBracket){
-        ArrayList<Team> teams = getAllTeams(curBracket);
+        ArrayList<Team> teams = curBracket.getTeams();
         for(Team team: teams){
             if(team.getTeamName().substring(0, 9).equals("BlankTeam")){
                 return team;
@@ -60,8 +35,8 @@ public class teamCreationUC {
         return null;
     }
 
-    // creates a new team and add the new team to the bracket based on user input
-    public Team createTeam(String teamName, int teamSize, DefaultUser creator, DefaultBracket curBracket){
+    // creates the team and returns a string that indicates whether the team has been successfully created
+    public String createTeam(String teamName, int teamSize, DefaultUser creator, DefaultBracket curBracket){
         boolean userIsPlayer = checkPlayer(creator, curBracket);
         boolean validTeamName = !checkTeamNameExists(teamName, curBracket);
         boolean spaceAvailable = findBlankTeam(curBracket) != null;
@@ -70,10 +45,14 @@ public class teamCreationUC {
             newTeam.setTeamName(teamName);
             newTeam.setTeamSize(teamSize);
             newTeam.addTeamMember(creator);
-            curBracket.addTeam(newTeam);
-            return newTeam;
+            return "Your team has been successfully created.";
+        } else if(!spaceAvailable){
+            return "This bracket is full and you can no longer create a new team.";
+        }else if (!validTeamName){
+            return "This team name has been used by another team, please enter a new team name.";
+        } else {
+            return "Only players can create a team.";
         }
-        return null;
     }
 
 }
