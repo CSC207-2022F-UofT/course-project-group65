745e3a2a073ac57d2065710bd562319a6754d080
Kelly Ou <kelleo0703@gmail.com>
2022-11-12 14:30:27 -0500

    Implemented teamCreation IB, ID, OD, OB.

--- src/main/java/useCases/teamCreation/teamCreationUC.java
+++ src/main/java/useCases/teamCreation/teamCreationUC.java
@@ -6,16 +6,24 @@ import entities.*;
 
 
 
-public class teamCreationUC {
+public class teamCreationUC implements teamCreationIB {
+    final teamCreationOB outputBoundary;
 
+    public teamCreationUC(teamCreationOB outputBoundary){
+        this.outputBoundary = outputBoundary;
+    }
 
-    public boolean checkPlayer(DefaultUser creator, DefaultBracket curBracket){
+    public boolean checkPlayer(teamCreationID userInput){
+        User creator = userInput.getUser();
+        Bracket curBracket = userInput.getBracket();
         return creator.getBracketRole(curBracket.getTournamentID()).equals("Player");
     }
 
     //used to check if a team with the same teamName exists in the bracket
-    public boolean checkTeamNameExists(String teamName, DefaultBracket curBracket){
-        ArrayList<Team> teams = curBracket.getTeams();
+    public boolean checkTeamNameExists(teamCreationID userInput){
+        String teamName = userInput.getTeamName();
+        Bracket curBracket = userInput.getBracket();
+        List<Team> teams = curBracket.getTeams();
         for(Team team: teams){
             if(team.getTeamName().equals(teamName)){
                 return false;
@@ -25,8 +33,9 @@ public class teamCreationUC {
     }
 
     //find a blank team in the bracket, returns null if the bracket is full
-    public Team findBlankTeam(DefaultBracket curBracket){
-        ArrayList<Team> teams = curBracket.getTeams();
+    public Team findBlankTeam(teamCreationID userInput){
+        Bracket curBracket = userInput.getBracket();
+        List<Team> teams = curBracket.getTeams();
         for(Team team: teams){
             if(team.getTeamName().substring(0, 9).equals("BlankTeam")){
                 return team;
@@ -36,23 +45,31 @@ public class teamCreationUC {
     }
 
     // creates the team and returns a string that indicates whether the team has been successfully created
-    public String createTeam(String teamName, int teamSize, DefaultUser creator, DefaultBracket curBracket){
-        boolean userIsPlayer = checkPlayer(creator, curBracket);
-        boolean validTeamName = !checkTeamNameExists(teamName, curBracket);
-        boolean spaceAvailable = findBlankTeam(curBracket) != null;
-        if(userIsPlayer && validTeamName && spaceAvailable){
-            Team newTeam = findBlankTeam(curBracket);
-            newTeam.setTeamName(teamName);
-            newTeam.setTeamSize(teamSize);
-            newTeam.addTeamMember(creator);
-            return "Your team has been successfully created.";
-        } else if(!spaceAvailable){
-            return "This bracket is full and you can no longer create a new team.";
-        }else if (!validTeamName){
-            return "This team name has been used by another team, please enter a new team name.";
-        } else {
-            return "Only players can create a team.";
-        }
+    public String createTeam(teamCreationID userInput){
+        String teamName = userInput.getTeamName();
+        int teamSize = userInput.getTeamSize();
+        User creator = userInput.getUser();
+        Team newTeam = findBlankTeam(userInput);
+        newTeam.setTeamName(teamName);
+        newTeam.setTeamSize(teamSize);
+        newTeam.addTeamMember(creator);
+        return "Your team has been successfully created.";
+
     }
 
+    @Override
+    public teamCreationOD createNewTeam(teamCreationID userInput) {
+        if (checkTeamNameExists(userInput)) {
+            return outputBoundary.prepareFailView("Team already exists.");
+        } else if (findBlankTeam(userInput) == null) {
+            return outputBoundary.prepareFailView("The bracket is full, please join an existing team.");
+        } else if (!checkPlayer(userInput)){
+            return outputBoundary.prepareFailView("Only players can create a new team.");
+        }
+
+        String success = createTeam(userInput);
+
+        teamCreationOD outputData = new teamCreationOD(userInput.getBracket(), success);
+        return outputBoundary.prepareSuccessView(outputData);
+    }
 }
