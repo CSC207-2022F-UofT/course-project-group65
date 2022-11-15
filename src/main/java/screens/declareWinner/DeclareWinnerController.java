d8d7fe5cb6d326ab13ca2b2184a10d80cd39f0bf
AdK0101 <aditya.khan@mail.utoronto.ca>
2022-11-15 02:33:05 -0500

    Implemented data persistence, refactored code, added documentation.

--- src/main/java/screens/declareWinner/DeclareWinnerController.java
+++ src/main/java/screens/declareWinner/DeclareWinnerController.java
@@ -1,25 +1,32 @@
 package screens.declareWinner;
-import entities.*;
-import useCases.declareWinner.*;
+
+import useCases.declareWinner.DeclareWinnerIB;
+import useCases.declareWinner.DeclareWinnerID;
+import useCases.declareWinner.DeclareWinnerOD;
 
 public class DeclareWinnerController {
 
-        DeclareWinnerIB userInput; // This is actually a use case instance. But it is of type DeclareWinnerIB for CA purposes.
+    DeclareWinnerIB userInput; // This is actually a use case instance. But it is of type DeclareWinnerIB for CA purposes.
 
-        public DeclareWinnerController(DeclareWinnerIB userInput) {
-            this.userInput = userInput;
-        }
+    /**
+     * Construct a DeclareWinnerController instance with the given DeclareWinnerIB.
+     *
+     * @param userInput The DeclareWinnerIB to use
+     */
 
-        // Prepare the output data for the view
-        public DeclareWinnerOD create(int bracketID, String username, int gameID, BracketRepo bracketRepo,
-                                    AccountRepo accountRepo){
+    public DeclareWinnerController(DeclareWinnerIB userInput) {
+        this.userInput = userInput;
+    }
 
-            // the input data is created.
-            DeclareWinnerID inputData = new DeclareWinnerID(bracketID, username, gameID, bracketRepo, accountRepo);
+    // Prepare the output data for the view
+    public DeclareWinnerOD create(int bracketID, String username, int gameID){
 
-            // this simultaneously runs the use case (by passing in ID), and returns the output data
-            // This is not using anything rn because we are using the presenter instead.
-            return this.userInput.setWinner(inputData);
-        }
+        // the input data is created.
+        DeclareWinnerID inputData = new DeclareWinnerID(bracketID, username, gameID);
+
+        // this simultaneously runs the use case (by passing in ID), and returns the output data
+        // This is not using anything rn because we are using the presenter instead.
+        return this.userInput.setWinner(inputData);
+    }
 
 }
--- src/main/java/useCases/declareWinner/DeclareWinnerID.java
+++ src/main/java/useCases/declareWinner/DeclareWinnerID.java
@@ -1,24 +1,15 @@
 package useCases.declareWinner;
 
-import entities.AccountRepo;
-import entities.Bracket;
-import entities.BracketRepo;
-import entities.User;
-
 public class DeclareWinnerID {
 
     private int bracketID;
     private String username;
     private int gameID;
-    private BracketRepo bracketRepo;
-    private AccountRepo accountRepo;
 
-    public DeclareWinnerID(int bracketID, String username, int gameID, BracketRepo bracketRepo, AccountRepo accountRepo) {
+    public DeclareWinnerID(int bracketID, String username, int gameID) {
         this.bracketID = bracketID;
         this.username = username;
         this.gameID = gameID;
-        this.bracketRepo = bracketRepo;
-        this.accountRepo = accountRepo;
     }
 
     public void setBracketID(int bracketID) {
@@ -37,12 +28,12 @@ public class DeclareWinnerID {
         return gameID;
     }
 
-    public Bracket getBracketDW() {
-        return bracketRepo.getBracket(this.bracketID);
+    public int getBracketIDDW() {
+        return bracketID;
     }
 
-    public User getUserDW() {
-        return accountRepo.getUser(this.username);
+    public String getUsernameDW() {
+        return username;
     }
 
 }
