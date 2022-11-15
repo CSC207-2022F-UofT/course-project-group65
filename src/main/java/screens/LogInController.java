2ab80b8d43fce831e9d37ef6ea70fb79ca5b40a2
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-11-14 22:15:54 -0500

    Created controller, not finished

---
+++ src/main/java/screens/LogInController.java
@@ -0,0 +1,21 @@
+package screens;
+
+import entities.AccountRepo;
+import entities.BracketRepo;
+import useCases.LogIn.LogInIB;
+import useCases.LogIn.LogInID;
+import useCases.LogIn.LogInUC;
+
+public class LogInController {
+    private final LogInIB logInIB;
+
+    public LogInController(LogInIB logInIB) {
+        this.logInIB = logInIB;
+    }
+
+    public void runLogin(LogInID requestModel, String username, String password, AccountRepo data, BracketRepo bracketData) {
+        // Note: hands off the work to the use case class.
+        LogInUC.LoginResult result = logInIB.logIn(requestModel, username, password, data, bracketData);
+
+    }
+}
