b88481738b811f208afd507eb14f1957caaec751
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-11-17 18:55:40 -0500

    implemented login and createaccount controllers and presenters

---
+++ src/main/java/screens/createAccount/CreateAccountController.java
@@ -0,0 +1,19 @@
+package screens.createAccount;
+
+import entities.AccountRepo;
+import useCases.CreateAccount.CreateAccountIB;
+import useCases.CreateAccount.CreateAccountID;
+import useCases.CreateAccount.CreateAccountOD;
+
+public class CreateAccountController {
+    private CreateAccountIB createAccountIB;
+
+    public CreateAccountController(CreateAccountIB createAccountIB) {
+        this.createAccountIB = createAccountIB;
+    }
+
+    public CreateAccountOD create(String username, String password, AccountRepo accountRepo) {
+        CreateAccountID createAccountID = new CreateAccountID(username, password);
+        return createAccountIB.create(createAccountID, accountRepo);
+    };
+}
