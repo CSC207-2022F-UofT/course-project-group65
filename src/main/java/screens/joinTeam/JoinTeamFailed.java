24c8dee54da2a8cb729e2ba5bd1876772115c3b6
Hanli <hhanli.jiang@mail.utoronto.ca>
2022-11-18 17:32:20 -0500

    Changed joinTeam

--- src/main/java/screens/joinTeam/JoinTeamFailed.java
+++ src/main/java/screens/joinTeam/JoinTeamFailed.java
@@ -1,5 +1,8 @@
 package screens.joinTeam;
 
+/**
+ * A class for the output data of the join team use case.
+ */
 public class JoinTeamFailed extends RuntimeException{
     public JoinTeamFailed(String error){
         super(error);
