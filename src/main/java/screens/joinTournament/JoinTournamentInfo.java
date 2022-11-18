4b19e679e05811d0008530bc218c95e0feb7aea3
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2022-11-18 01:08:40 -0500

    Implemented Info files for join and view tournament

--- src/main/java/screens/joinTournament/JoinTournamentInfo.java
+++ src/main/java/screens/joinTournament/JoinTournamentInfo.java
@@ -1,4 +1,25 @@
 package screens.joinTournament;
 
-public class JoinTournamentInfo {
+import javax.swing.*;
+
+public class JoinTournamentInfo extends JFrame{
+    private JPanel joinTournament;
+    private JButton btSubmit;
+    private JTextField tfInvite;
+    private JLabel lbInvite;
+
+    public JoinTournamentInfo(){
+        setContentPane(joinTournament);
+        setTitle("Join Tournament");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btSubmit.addActionListener(e -> {
+            String invite = tfInvite.getText();
+        });
+    }
+
+    public static void main(String[] args) {
+        JoinTournamentInfo joinTournamentInfo = new JoinTournamentInfo();
+    }
 }
--- src/main/java/screens/viewTournament/ViewTournamentInfo.java
+++ src/main/java/screens/viewTournament/ViewTournamentInfo.java
@@ -1,4 +1,21 @@
 package screens.viewTournament;
 
-public class ViewTournamentInfo {
+import javax.swing.*;
+
+public class ViewTournamentInfo extends JFrame{
+    private JPanel viewTournament;
+    private JButton btSubmit;
+    private JLabel lbTournamentID;
+    private JComboBox cbTournamentID;
+
+    public ViewTournamentInfo(){
+        setContentPane(viewTournament);
+        setTitle("View Tournament");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btSubmit.addActionListener(e -> {
+            int TournamentID = (Integer) cbTournamentID.getSelectedItem();
+        });
+    }
 }
