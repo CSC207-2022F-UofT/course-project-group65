9002eacfd458e685583a081638d88ec324b7d042
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-11-18 21:56:30 -0500

    fixed creating user

--- src/main/java/entities/DefaultUserFactory.java
+++ src/main/java/entities/DefaultUserFactory.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 public class DefaultUserFactory implements UserFactory{
     @Override
     public User create(String username, String password, HashMap<Integer, String> bracketRole, int currentTournament, ArrayList<Integer> currentTournaments) {
-        return new DefaultUser();
+        DefaultUser defaultUser = new DefaultUser();
+        defaultUser.setUsername(username);
+        defaultUser.setPassword(password);
+
+        return defaultUser;
     }
 }
