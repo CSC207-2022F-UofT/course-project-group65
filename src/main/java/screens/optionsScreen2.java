34e1759bbc1fdb2e9ea60b21de2980aa7b28622c
Kelly Ou <kelleo0703@gmail.com>
2022-11-18 19:05:23 -0500

    Changed UserInputUC (uncommented actionPerformed). Changed optionsScreen2 button listeners.

--- src/main/java/screens/optionsScreen2.java
+++ src/main/java/screens/optionsScreen2.java
@@ -1,14 +1,21 @@
 package screens;
 
+import entities.UserFactory;
+import screens.teamCreation.UserInput;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class optionsScreen2 extends JFrame{
-    private JButton joinAnExistingTeamButton;
-    private JButton createANewTeamButton;
-    private JButton backToPreviousScreenButton;
+
+public class optionsScreen2 extends JFrame {
+    private JButton btJoinTeam;
+    private JButton btCreateTeam;
+    private JButton btPrevious;
     private JPanel optionsScreen2;
+
 
     public optionsScreen2() {
         setContentPane(optionsScreen2);
@@ -17,27 +24,24 @@ public class optionsScreen2 extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
-//        createANewTeamButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//
-//            }
-//        });
-//        joinAnExistingTeamButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//
-//            }
-//        });
-//        backToPreviousScreenButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//
-//            }
-//        });
+        btJoinTeam.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                dispose();
+                optionsScreen2.setVisible(false);
+//                JFrame createTeam = new UserInput();
+//                createTeam.setVisible(true);
+            }
+        });
+        btCreateTeam.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                optionsScreen2.setVisible(false);
+//                JFrame joinTeam = new bracketView();
+//                joinTeam.setVisible(true);
+            }
+        });
 
-    }
-    public static void main(String[] args) {
-        optionsScreen2 optionsScreen2 = new optionsScreen2();
+
     }
 }
