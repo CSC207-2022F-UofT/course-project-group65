d895b8911b7535148b40bec9931d51ccbdc36926
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2022-11-25 15:12:38 -0500

    adding exception for joinTournament

--- src/main/java/useCases/joinTournament/JoinTournamentUC.java
+++ src/main/java/useCases/joinTournament/JoinTournamentUC.java
@@ -35,31 +35,36 @@ public class JoinTournamentUC implements JoinTournamentIB{
     public JoinTournamentOD joinBracket(JoinTournamentID input){
         String role = input.getInvite().substring(0, 2);
         String idAndName = input.getInvite().substring(2);
-        int tournamentID = Integer.parseInt(idAndName.split("(?<=\\d)(?=\\D)")[0]);
+        try {
+            int tournamentID = Integer.parseInt(idAndName.split("(?<=\\d)(?=\\D)")[0]);
 
-        if (!bracketRepo.getBrackets().containsKey(tournamentID)){
-            return outputBound.prepareFailView("Tournament does not exist.");
+            if (!bracketRepo.getBrackets().containsKey(tournamentID)){
+                return outputBound.prepareFailView("Tournament does not exist.");
+            }
+            else if (currUser.getAllTournaments().contains(tournamentID)){
+                return outputBound.prepareFailView("You have already joined this tournament.");
+            }
+            else if (!role.equals("PL") && !role.equals("OB")){
+                return outputBound.prepareFailView("Role does not exist within tournament.");
+            }
+            currUser.setCurrentTournament(tournamentID);
+            currUser.addTournament(tournamentID);
+            JoinTournamentOD output;
+            if (role.equals("PL")){
+                currUser.setBracketRole(tournamentID, "Player");
+                output = new JoinTournamentOD(currUser.getUsername(), accountRepo, bracketRepo,
+                        tournamentID, "Player");
+            }
+            else {
+                currUser.setBracketRole(tournamentID, "Observer");
+                bracketRepo.getBracket(tournamentID).addReferee(currUser);
+                output = new JoinTournamentOD(currUser.getUsername(), accountRepo, bracketRepo,
+                        tournamentID, "Observer");
+            }
+            return outputBound.prepareSuccessView(output);
         }
-        else if (currUser.getAllTournaments().contains(tournamentID)){
-            return outputBound.prepareFailView("You have already joined this tournament.");
+        catch (NumberFormatException nex){
+            return outputBound.prepareFailView("Invalid invite format");
         }
-        else if (!role.equals("PL") && !role.equals("OB")){
-            return outputBound.prepareFailView("Role does not exist within tournament.");
-        }
-        currUser.setCurrentTournament(tournamentID);
-        currUser.addTournament(tournamentID);
-        JoinTournamentOD output;
-        if (role.equals("PL")){
-            currUser.setBracketRole(tournamentID, "Player");
-            output = new JoinTournamentOD(currUser.getUsername(), accountRepo, bracketRepo,
-                    tournamentID, "Player");
-        }
-        else {
-            currUser.setBracketRole(tournamentID, "Observer");
-            bracketRepo.getBracket(tournamentID).addReferee(currUser);
-            output = new JoinTournamentOD(currUser.getUsername(), accountRepo, bracketRepo,
-                    tournamentID, "Observer");
-        }
-        return outputBound.prepareSuccessView(output);
     }
 }
