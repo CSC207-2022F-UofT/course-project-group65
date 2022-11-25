5ac052fc8fa82ede52bb5d539a35bdc352cba9de
Nicknames43 <nicholas.pedro@mail.utoronto.ca>
2022-11-25 15:31:17 -0500

    create empty bracketview after joining tournament

--- src/main/java/screens/joinTournament/JoinTournamentInfo.java
+++ src/main/java/screens/joinTournament/JoinTournamentInfo.java
@@ -1,7 +1,26 @@
 package screens.joinTournament;
 
+import entities.Bracket;
+import screens.NextScreenData;
+import screens.bracketView;
+import screens.endTourn.EndTournController;
+import screens.endTourn.EndTournPresenter;
+import screens.joinTeam.JoinTeamController;
+import screens.joinTeam.JoinTeamPresenter;
 import screens.optionsScreen2;
+import screens.startTourn.StartTournController;
+import screens.startTourn.StartTournPresenter;
+import useCases.endTourn.EndTournIB;
+import useCases.endTourn.EndTournOB;
+import useCases.endTourn.EndTournUC;
+import useCases.joinTeam.JoinTeamIB;
+import useCases.joinTeam.JoinTeamOB;
+import useCases.joinTeam.JoinTeamUC;
 import useCases.joinTournament.JoinTournamentOD;
+import useCases.startTourn.StartTournIB;
+import useCases.startTourn.StartTournOB;
+import useCases.startTourn.StartTournUC;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -28,7 +47,38 @@ public class JoinTournamentInfo extends JFrame implements ActionListener {
         String invite = tfInvite.getText();
         try{
             JoinTournamentOD outData = controller.joinTournament(invite);
+
+            EndTournOB endTournOB = new EndTournPresenter();
+            EndTournIB endTournIB = new EndTournUC(endTournOB, outData.getUsername(), outData.getAccounts(),
+                    outData.getBrackets(), outData.getTournamentID());
+            EndTournController endTournController = new EndTournController(endTournIB);
+
+            StartTournOB startTournOB = new StartTournPresenter();
+            StartTournIB startTournIB = new StartTournUC(startTournOB, outData.getUsername(), outData.getAccounts(),
+                    outData.getBrackets(), outData.getTournamentID());
+            StartTournController startTournController = new StartTournController(startTournIB);
+
+            JoinTeamOB joinTeamOB = new JoinTeamPresenter();
+            JoinTeamIB joinTeamIB = new JoinTeamUC(joinTeamOB, outData.getUsername(), outData.getTournamentID(),
+                    outData.getAccounts(), outData.getBrackets());
+            JoinTeamController joinTeamController = new JoinTeamController(joinTeamIB);
+
+            NextScreenData nextScreenData = new NextScreenData();
+            nextScreenData.setBrackets(outData.getBrackets());
+            nextScreenData.setAccounts(outData.getAccounts());
+            nextScreenData.setCurrentUser(outData.getUsername());
+            nextScreenData.setCurrentBracketID(outData.getTournamentID());
+
+            bracketView view = new bracketView(nextScreenData, endTournController, startTournController, joinTeamController);
+            Bracket bracket = outData.getBrackets().getBracket(outData.getTournamentID());
+            view.setBracketName(bracket.getTournamentName());
+            view.setCurrentUser(outData.getUsername());
+            view.setCurrentTournament(outData.getTournamentID());
+            view.setPlayerInvite(bracket.getPlayerInvite());
+            view.setObserverInvite(bracket.getObserverInvite());
+
             dispose();
+            view.setVisible(true);
         }
         catch (JoinTournamentFailed ex){
             JOptionPane.showMessageDialog(this, ex.getMessage());
