e0e4b6bd8021fa7b605e0dbb286fbe2da9884621
AdK0101 <aditya.khan@mail.utoronto.ca>
2022-11-25 19:25:23 -0500

    Made changes to main file

--- src/main/java/TournamentSimulator.java
+++ src/main/java/TournamentSimulator.java
@@ -1,9 +1,7 @@
-import entities.AccountRepo;
-import entities.BracketRepo;
-import entities.DefaultUserFactory;
-import entities.UserFactory;
+import entities.*;
 import screens.createAccount.CreateAccountController;
 import screens.createAccount.CreateAccountPresenter;
+import screens.createBracket.CreateBracketPresenter;
 import screens.homeScreen;
 import screens.logIn.LogInController;
 import screens.logIn.LogInPresenter;
@@ -13,15 +11,48 @@ import useCases.CreateAccount.CreateAccountUC;
 import useCases.LogIn.LogInIB;
 import useCases.LogIn.LogInOB;
 import useCases.LogIn.LogInUC;
+import useCases.createBracket.CreateBracketOB;
+import useCases.createBracket.CreateBracketUC;
 
 public class TournamentSimulator {
     public static void main(String[] args) {
         AccountRepo mainAccountRepo = new AccountRepo();
         BracketRepo mainBracketRepo = new BracketRepo();
-        UserFactory mainUserFactory = new DefaultUserFactory();
+
+//        DefaultBracket defaultBracket = new DefaultBracket();
+//        defaultBracket.setTournamentName("test");
+//        defaultBracket.setPlayerInvite();
+//        mainBracketRepo.addBracket(defaultBracket);
+//
+//        Team team1 = new DefaultTeam();
+//        team1.setTeamName("test 1");
+//        Team team2 = new DefaultTeam();
+//        team2.setTeamName("test 2");
+//        Team team3 = new DefaultTeam();
+//        team3.setTeamName("test 3");
+//        Team team4 = new DefaultTeam();
+//        team4.setTeamName("test 4");
+//
+//        team1.setTeamSize(2);
+//        team2.setTeamSize(2);
+//        team3.setTeamSize(2);
+//        team4.setTeamSize(2);
+//
+//        defaultBracket.addTeam(team1);
+//        defaultBracket.addTeam(team2);
+//        defaultBracket.addTeam(team3);
+//        defaultBracket.addTeam(team4);
+
+//        CreateBracketOB presenter = new CreateBracketPresenter();
+//
+//        CreateBracketUC createBracketUC = new CreateBracketUC(presenter, mainBracketRepo);
+
+//        UserFactory mainUserFactory = new DefaultUserFactory();
 
         CreateAccountOB createAccountOB = new CreateAccountPresenter();
-        CreateAccountIB createAccountIB = new CreateAccountUC(createAccountOB, mainUserFactory,
+//        CreateAccountIB createAccountIB = new CreateAccountUC(createAccountOB, mainUserFactory,
+//                mainAccountRepo, mainBracketRepo);
+        CreateAccountIB createAccountIB = new CreateAccountUC(createAccountOB,
                 mainAccountRepo, mainBracketRepo);
         CreateAccountController createAccountController = new CreateAccountController(createAccountIB);
 
