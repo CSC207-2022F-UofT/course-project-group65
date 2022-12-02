3f5e89cecb47402c4cfb0da0007881d4547bfdab
david <mjdavid.park@mail.utoronto.ca>
2022-12-02 15:33:09 -0500

    added javadoc for Start/EndTourn DSIDs, Gateways.

--- src/main/java/useCases/endTourn/EndTournGateway.java
+++ src/main/java/useCases/endTourn/EndTournGateway.java
@@ -3,6 +3,12 @@ package useCases.endTourn;
 
 import java.io.IOException;
 
+/** An interface for the gateway of the end tournament use case.
+ * This interface is used for dependency inversion, allowing the use case to be independent of the database.
+ */
 public interface EndTournGateway {
+    /** Saves the data to the database
+     * @param data - the data source interface data
+     */
     void save(EndTournDSID data) throws IOException;
 }
