5e0f509283a35ddc2dd68239f5445109794553c2
SpaceJesusTM <sugihikoruo@gmail.com>
2022-12-02 13:36:36 -0500

    Finished all comments for CreateBracket.

--- src/main/java/useCases/createBracket/DefaultGamesFactory.java
+++ src/main/java/useCases/createBracket/DefaultGamesFactory.java
@@ -6,11 +6,29 @@ import entities.Team;
 
 import java.util.*;
 
+/**
+ * This class represents the default games factory. This class is used to create
+ * a default game which simply keeps track of the teams that are playing in the game, the points
+ * of each team, and the winner.
+ * This class is part of the factory design pattern, and used in the builder design pattern giving the class
+ * much flexibility, allowing us to create games of arbitrary sizes.
+ * Note: In our program, the games are the main structure of the actual bracket itself, and are represented
+ * in the form of a tree (where the root of the tree/head game represents the final game).
+ * When first created all the games that will be used in the tournament are created
+ * and default teams are assigned to the leaves (starting games) of the tree. These values are updated
+ * as new teams are created, new users join, and as the tournament progresses.
+ * This means the default teams have to be created before the games are created.
+ */
 public class DefaultGamesFactory implements GamesFactory {
-    /*
-     * This is a factory for creating a game.
-     */
 
+    /**
+     * A helper method for the getGames method.
+     * This method is used to create and insert a new game into the tree/bracket.
+     * @param root - the root of the tree/bracket (head game, final round of the bracket)
+     * @param id - the unique ID of the game (each game in the bracket has a unique ID)
+     *           (Note: this is not the same as the unique ID of the bracket, as games in different brackets
+     *           may have the same ID. As long as with in the same bracket, the IDs are unique).
+     */
     public void insertValue(Game root, int id) {
         Queue<Game> queue = new LinkedList<>();
         queue.add(root);
@@ -38,6 +56,13 @@ public class DefaultGamesFactory implements GamesFactory {
         }
     }
 
+    /** A helper method for the getGames method.
+     * This method is used to update the round parameter in each game, so
+     * that it refers to the correct round of the bracket the game is in.
+     * The round a game is in is analogous to the height of the game in the tree/bracket.
+     * @param root - the root of the tree/bracket (head game, final round of the bracket)
+     * @param rounds - the total number of rounds in the bracket, equivalent to the height of the tree
+     */
     public void setRounds(Game root, int rounds){
         if (root.getPrevGame1() == null && root.getPrevGame2() == null)
             root.setGameRound(rounds);
@@ -48,6 +73,12 @@ public class DefaultGamesFactory implements GamesFactory {
         }
     }
 
+    /** A helper method for the getGames method.
+     * This method is used to put the default teams into the starting games of the bracket.
+     * (This is the first round of the bracket, which are the leaves of the tree).
+     * @param root - the root of the tree/bracket (head game, final round of the bracket)
+     * @param teams - the list of teams in the bracket
+     */
     public void setTeams(Game root, List<Team> teams){
         if (root.getPrevGame1() == null && root.getPrevGame2() == null) {
             root.setTeam(teams.get(1), 0);
@@ -62,6 +93,14 @@ public class DefaultGamesFactory implements GamesFactory {
         }
     }
 
+    /**
+     * This is the main method which creates all the games in the bracket based on the number of teams.
+     * It also correctly assigns all the teams to the starting games of the bracket, and updates the round
+     * parameter in each game, so we can identify all the games in a specific round of the bracket.
+     * @param numTeams - the number of teams in the bracket
+     * @param teams - the list of teams in the bracket
+     * @return - the head game of the bracket (the final game of the bracket)
+     */
     public DefaultGame getGames(int numTeams, List<Team> teams)
     {
         DefaultGame root = new DefaultGame();
--- src/main/java/useCases/createBracket/GamesFactory.java
+++ src/main/java/useCases/createBracket/GamesFactory.java
@@ -5,10 +5,16 @@ import entities.Team;
 
 import java.util.List;
 
+/** An interface for a games factory.
+ * It is part of the factory design pattern, and in our program is implemented
+ * by the DefaultGamesFactory class.
+ */
 public interface GamesFactory {
-    /*
-     * This is a factory for creating a game.
-     */
 
+    /** A method to create the games tree for the bracket.
+     * @param numTeams - the number of teams in the tournament (determines number of games)
+     * @param teams - the list of teams in the tournament
+     * @return the head game of the bracket
+     */
     Game getGames(int numTeams, List<Team> teams);
 }
