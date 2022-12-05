5d70c86927d49720e9139abe162dfc9cde7bfbcb
Kelly Ou <kelleo0703@gmail.com>
2022-12-05 15:12:18 -0500

    Completed java docs for user input screen for teamCreation use case

--- src/main/java/frameworks_and_drivers/UserInput.java
+++ src/main/java/frameworks_and_drivers/UserInput.java
@@ -9,16 +9,31 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This is the screen that takes in user input after the user clicks create in the team view page
+ * It takes in a team name as a string from the text field and initializes the controller for team
+ * creation use case once the create button is clicked.
+ */
 public class UserInput extends JFrame implements ActionListener {
     private JLabel TeamName;
+    /** This is the text field that takes in a team name from the user  */
     private JTextField tfTeamName;
+    /** This is the button that the user clicks when they finish */
     private JButton btnCreate;
     private JPanel inputPanel;
+    /** This is the controller that is connected to the create button */
     private final TeamCreationController teamCreationController;
-//    public bracketView view;
+    /** This contains all the data that was passed on from previous screens */
     private final NextScreenData nextScreenData;
+    /** This is the view model that gets updated after teamCreation gets run */
     public ExtendedView extendedView;
 
+    /**
+     * This creates an instance of UserInput screen and makes the screen visible to the user
+     * @param controller teamCreation controller that passes input to the interactor
+     * @param viewChange this is the current view model that will get updated
+     * @param nextScreenData this contains all the data that was passed on from pervious screens
+     */
     public UserInput(TeamCreationController controller, ExtendedView viewChange, NextScreenData nextScreenData) {
 
         this.teamCreationController = controller;
@@ -32,6 +47,11 @@ public class UserInput extends JFrame implements ActionListener {
         btnCreate.addActionListener(this);
     }
 
+    /**
+     * This reacts to the button and calls the creatNewTeam method to run the teamCreation use case after taking in
+     * a valid team name.
+     * @param evt the event to be processed
+     */
     public void actionPerformed(ActionEvent evt) {
 
         try {
