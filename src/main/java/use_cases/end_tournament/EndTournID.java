98fec680c3a2069f31f8a4212daea63ab9c350af
david <mjdavid.park@mail.utoronto.ca>
2022-12-05 15:39:07 -0500

    Cleaned up code for Start/EndTourn

--- src/main/java/use_cases/end_tournament/EndTournID.java
+++
@@ -1,9 +0,0 @@
-package use_cases.end_tournament;
-
-
-/**
- * The input data class for the start bracket input boundary.
- */
-public class EndTournID {
-}
-
--- src/main/java/use_cases/start_tournament/StartTournID.java
+++
@@ -1,8 +0,0 @@
-package use_cases.start_tournament;
-
-
-/**
- * The input data class for the start bracket input boundary.
- */
-public class StartTournID {
-}
